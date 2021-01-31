20ad0fb7065fc9e93541ce586e2a836dd19c22c0
Josh Kuttler <yoshuakuttler@gmail.com>
2021-01-31 23:16:11 +0200

    update

--- extensions/netflux-react-native/jest/jest.config.js
+++ extensions/netflux-react-native/jest/jest.config.js
@@ -1,11 +1,7 @@
 const merge = require('lodash.merge');
 const {jestConfig} = require('@teambit/react-native');
 
-const config = {
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|react-native-button|@netflux)/)',
-  ],
-};
+const config = {};
 
 const mergedConfig = merge(jestConfig, config);
 
